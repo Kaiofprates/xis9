214936185a0691a64482b595dceffb7907def665
kaiofprates <kaioprates@hotmail.com>
2020-04-16 20:35:16 -0300

    add functions delete and modal in gallery

--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {
+    useState,
+    useEffect
+} from 'react'
 import Banner from '../../components/Perfil'
 import { Chip, Searchbar } from 'react-native-paper';
 
-import { getDataBase } from '../../Database/data';
+import { getDataBase, deletData } from '../../Database/data';
 
 import {
 
@@ -10,7 +13,8 @@ import {
     TouchableOpacity,
     Text,
     SafeAreaView,
-    View
+    View,
+    Modal
 
 } from 'react-native';
 
@@ -18,16 +22,18 @@ import styles from './styles';
 
 export default function Gallery() {
 
-    const dados = [
-        {
-            nome: 'Kaio',
-            photo: 'file:///data/data/host.exp.exponent/cache/ExperienceData/%2540kaiofprates%252Fxnine/Camera/3bd29b21-c11a-482d-8346-902f35bc2fbc.jpg'
-        },
-        {
-            nome: 'Kaio',
-            photo: 'file:///data/data/host.exp.exponent/cache/ExperienceData/%2540kaiofprates%252Fxnine/Camera/3bd29b21-c11a-482d-8346-902f35bc2fbc.jpg'
+    const [data, setData] = useState([]);
+    const [modal, setModal] = useState(false);
+
+    useEffect(() => {
+        async function sendData() {
+            await getDataBase(setData);
+            console.log(data.items[1]);
         }
-    ]
+        sendData();
+
+    }, [])
+
 
 
     function teste() {
@@ -38,16 +44,17 @@ export default function Gallery() {
     renderItem = ({ item }) => (
         <TouchableOpacity
             onPress={() => {
-                alert('Hello World!')
+                //deletData(item.nome);
+                setModal(!modal);
             }}>
-            <Banner key={item} nome={null} url={item.photo} />
+            <Banner key={item.nome} nome={null} url={item.photo} />
+
             <View style={styles.info}>
-                <Chip icon="book" style={styles.chip} onPress={() => teste()} type="outlined" >Nome</Chip>
-                <Chip icon="home" style={styles.chip} onPress={() => console.log('Pressed')}>Endereço</Chip>
-                <Chip icon="gavel" style={styles.chip} onPress={() => console.log('Pressed')}>Artigo</Chip>
+                <Chip icon="book" style={styles.chip} onPress={() => teste()} type="outlined" >{item.nome}</Chip>
+                <Chip icon="home" style={styles.chip} onPress={() => console.log('Pressed')}>{item.rua + '  ' + item.numero}</Chip>
+                <Chip icon="home" style={styles.chip} onPress={() => console.log('Pressed')}>{item.bairro}</Chip>
+                <Chip icon="gavel" style={styles.chip} onPress={() => console.log('Pressed')}>{item.artigo}</Chip>
             </View>
-
-
         </TouchableOpacity>
 
     );
@@ -59,9 +66,35 @@ export default function Gallery() {
             <Searchbar
                 placeholder="Search"
             />
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modal}
+                onRequestClose={() => {
+                    setModal();
+                }}
+
+
+            >
+                <View>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setModal(!modal);
+                        }}  >
+                        <Text style={{
+                            color: 'white',
+                            textSize: 40
+                        }}>
+                            Sair
+                      </Text>
+                    </TouchableOpacity>
+
+                </View>
+
+            </Modal>
             <FlatList
                 horizontal={true}
-                data={dados}
+                data={data.items}
                 renderItem={renderItem}
                 // numColumns={2}
                 style={styles.flat}
